4e56119248497df5782afc9ca50a484abcf2127a
Krzysiek <Krzysiek@DESKTOP-MGANA6A>
2022-12-30 20:25:13 +0100

    make it better

---
+++ src/main/java/com/example/demo/websocket/WebSocketHandler.java
@@ -0,0 +1,15 @@
+package com.example.demo.websocket;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+import java.io.IOException;
+
+public class WebSocketHandler extends AbstractWebSocketHandler {
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
+        String msg = String.valueOf(message.getPayload());
+        // Send back unique message depending on the id received from the client
+        System.out.println(msg);
+    }
+}
