c3e450e53598656d610fe806f7abfcf0a88a6ee7
Krzysiek <Krzysiek@DESKTOP-MGANA6A>
2023-01-02 14:05:32 +0100

    make it better 2

--- src/main/java/com/example/demo/esp/EspController.java
+++ src/main/java/com/example/demo/esp/EspController.java
@@ -1,6 +1,7 @@
 package com.example.demo.esp;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +9,7 @@ public class EspController {
 
 
     @PostMapping("/emergencyCall")
-    public void receiveCall(){
-        System.out.println("dostalem zgloszenie");
+    public void receiveCall(@RequestBody String body){
+        System.out.println("dostalem zgloszenie od "+ body);
     }
 }
--- src/main/java/com/example/demo/websocket/WebSocketHandler.java
+++ src/main/java/com/example/demo/websocket/WebSocketHandler.java
@@ -1,5 +1,7 @@
 package com.example.demo.websocket;
 
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
@@ -11,5 +13,10 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
         String msg = String.valueOf(message.getPayload());
         // Send back unique message depending on the id received from the client
         System.out.println(msg);
+    }
+
+    @SendTo("/websocket")
+    public void sendNotificationAboutEmergency(){
+
     }
 }
